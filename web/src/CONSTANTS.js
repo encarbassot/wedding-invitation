42d54f94fe01e303a78f7a9db51464c40b263d25
encarbassot <elgrefa@gmail.com>
2025-03-08 23:00:50 +0100

    style fix

--- web/src/CONSTANTS.js
+++ web/src/CONSTANTS.js
@@ -1,3 +1,5 @@
 export const API_URL = "http://192.168.31.21:3000"
 // export const API_URL = "http://localhost:3000"
 export const WEB_URL = "http://localhost:5174"
+//export const API_URL = "https://api.xavilaura.cat"
+// export const WEB_URL = "https://enscasem.xavilaura.cat"
